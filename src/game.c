38f58d4c0cf5f2334f8b965afba96969294c72fb
Todd Cope <xander4jesus@yahoo.com>
2014-06-26 16:55:56 -0500

    Stop timer while starting streams in case the take a bit to start. Should make timing more consistent.

--- src/game.c
+++ src/game.c
@@ -70,11 +70,13 @@ bool lss_game_initialize(LSS_GAME * gp, ALLEGRO_PATH * song_path)
 	gp->board_y = 420.0;
 	gp->board_speed = 12.0;
 	lss_initialize_player(gp, 0);
+	lss_song_mark_beats(gp->song, gp->song_audio->length);
+	al_stop_timer(t3f_timer);
 	if(!lss_set_song_audio_playing(gp->song_audio, true))
 	{
 		return false;
 	}
-	lss_song_mark_beats(gp->song, gp->song_audio->length);
+	al_start_timer(t3f_timer);
 	gp->done = false;
 	return true;
 }
