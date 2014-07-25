9a93cd4fef0b05f0b55ce9bd83c83f1b4fa14acb
Todd Cope <xander4jesus@yahoo.com>
2014-07-25 09:40:48 -0500

    Fix crash bug in game results screen.

--- src/state_game_results.c
+++ src/state_game_results.c
@@ -12,7 +12,14 @@ void lss_game_results_render(LSS_GAME * gp, LSS_RESOURCES * rp)
 	al_hold_bitmap_drawing(true);
 	al_draw_textf(rp->font[LSS_FONT_SMALL], t3f_color_white, 0, 0, 0, "Results for %s", gp->player[0].profile->name);
 	al_draw_textf(rp->font[LSS_FONT_SMALL], t3f_color_white, 0, 48, 0, "Score: %d", gp->player[0].score); 
-	al_draw_textf(rp->font[LSS_FONT_SMALL], t3f_color_white, 0, 72, 0, "Accuracy: %d", (gp->player[0].hit_notes * 100) / total_notes);
+	if(total_notes > 0)
+	{
+		al_draw_textf(rp->font[LSS_FONT_SMALL], t3f_color_white, 0, 72, 0, "Accuracy: %d", (gp->player[0].hit_notes * 100) / total_notes);
+	}
+	else
+	{
+		al_draw_textf(rp->font[LSS_FONT_SMALL], t3f_color_white, 0, 72, 0, "Accuracy: N/A");
+	}
 	al_draw_textf(rp->font[LSS_FONT_SMALL], t3f_color_white, 0, 96, 0, "High Score: %d", gp->player[0].high_score);
 	al_hold_bitmap_drawing(false);
 }
