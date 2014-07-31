2cc2725426a2d4018996cc1fa247b9887edd67c6
Todd Cope <xander4jesus@yahoo.com>
2014-07-31 17:30:32 -0500

    Add members for storing perfect and bad note counts for use in results screen.

--- src/struct_game.h
+++ src/struct_game.h
@@ -32,7 +32,7 @@ typedef struct
 	int life;
 	int miss_streak;
 	
-	int hit_notes, missed_notes;
+	int hit_notes, missed_notes, perfect_notes, bad_notes;
 
 	LSS_PLAYER_NOTE_GROUP next_notes;
 	LSS_PLAYER_NOTE_GROUP playing_notes;
