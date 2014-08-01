94eed83a549efb5584f4a8e762338ed54c8ac150
Todd Cope <xander4jesus@yahoo.com>
2014-08-01 10:18:46 -0500

    Stop song audio when game ends.

--- src/state_game.c
+++ src/state_game.c
@@ -208,6 +208,7 @@ void lss_game_logic(LSS_GAME * gp)
 	}
 	if(t3f_key[ALLEGRO_KEY_ESCAPE] || gp->player[0].life <= 0 || gp->current_tick >= gp->song_audio->length * 60.0)
 	{
+		lss_set_song_audio_playing(gp->song_audio, false);
 		lss_game_get_player_results(gp, 0);
 		gp->done = true;
 		t3f_key[ALLEGRO_KEY_ESCAPE] = 0;
