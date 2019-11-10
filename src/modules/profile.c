70ec3f3b8f8f7ff925e767f44737d5c2c15f1fcf
Todd Cope <xander4jesus@yahoo.com>
2019-11-10 08:32:20 -0600

    Lay some groundwork for career mode.

--- src/modules/profile.c
+++ src/modules/profile.c
@@ -2,13 +2,43 @@
 
 #include "profile.h"
 
+static bool lss_load_profile_career(LSS_PROFILE * pp, int career)
+{
+	char buf[1024];
+	char buf2[1024];
+	const char * val;
+	int i, j;
+
+	sprintf(buf, "Career_%d", career);
+	pp->career[career].name = al_get_config_value(pp->config, buf, "Name");
+	val = al_get_config_value(pp->config, buf, "Tiers");
+	if(val)
+	{
+		pp->career[career].tiers = atoi(val);
+	}
+	for(i = 0; i < pp->career[career].tiers; i++)
+	{
+		for(j = 0; j < LSS_PROFILE_CAREER_MAX_SONGS; j++)
+		{
+			sprintf(buf2, "Tier_%d_Passed_%d", i, j);
+			val = al_get_config_value(pp->config, buf, buf2);
+			if(val)
+			{
+				pp->career[career].tier[i].song_passed[j] = atoi(val);
+			}
+		}
+	}
+	return true;
+}
+
 LSS_PROFILES * lss_load_profiles(void)
 {
 	const ALLEGRO_FILE_INTERFACE * old_interface;
 	LSS_PROFILES * pp;
 	char fn[1024];
 	char buf[1024];
-	int i;
+	const char * val;
+	int i, j;
 
 	old_interface = al_get_new_file_interface();
 	al_set_standard_file_interface();
@@ -23,6 +53,15 @@ LSS_PROFILES * lss_load_profiles(void)
 			if(pp->entry[pp->entries].config)
 			{
 				pp->entry[pp->entries].name = al_get_config_value(pp->entry[pp->entries].config, "Settings", "Name");
+				val = al_get_config_value(pp->entry[pp->entries].config, "Settings", "Careers");
+				if(val)
+				{
+					pp->entry[pp->entries].careers = atoi(val);
+				}
+				for(j = 0; j < pp->entry[pp->entries].careers; j++)
+				{
+					lss_load_profile_career(&pp->entry[pp->entries], j);
+				}
 				pp->entries++;
 			}
 		}
@@ -41,17 +80,45 @@ LSS_PROFILES * lss_load_profiles(void)
 	return pp;
 }
 
+static void lss_save_profile_career(LSS_PROFILE * pp, int career)
+{
+	char buf[1024];
+	char buf2[1024];
+	char buf3[1024];
+	int i, j;
+
+	sprintf(buf, "Career_%d", career);
+	al_set_config_value(pp->config, buf, "Name", pp->career[career].name);
+	sprintf(buf2, "%d", pp->career[career].tiers);
+	al_set_config_value(pp->config, buf, "Tiers", buf2);
+	for(i = 0; i < pp->career[career].tiers; i++)
+	{
+		for(j = 0; j < LSS_PROFILE_CAREER_MAX_SONGS; j++)
+		{
+			sprintf(buf2, "Tier_%d_Passed_%d", i, j);
+			sprintf(buf3, "%d", pp->career[career].tier[i].song_passed[j]);
+			al_set_config_value(pp->config, buf, buf2, buf3);
+		}
+	}
+}
+
 bool lss_save_profiles(LSS_PROFILES * pp)
 {
 	const ALLEGRO_FILE_INTERFACE * old_interface;
 	char fn[1024];
 	char buf[1024];
-	int i;
+	int i, j;
 
 	old_interface = al_get_new_file_interface();
 	al_set_standard_file_interface();
 	for(i = 0; i < pp->entries; i++)
 	{
+		sprintf(buf, "%d", pp->entry[i].careers);
+		al_set_config_value(pp->entry[i].config, "Settings", "Careers", buf);
+		for(j = 0; j < pp->entry[i].careers; j++)
+		{
+			lss_save_profile_career(&pp->entry[i], j);
+		}
 		/* save config file */
 		sprintf(fn, "%s%d.dat", t3f_get_filename(t3f_data_path, "profile", buf, 1024), i);
 		al_save_config_file(fn, pp->entry[i].config);
--- src/modules/profile.h
+++ src/modules/profile.h
@@ -1,15 +1,36 @@
 #ifndef LSS_PROFILE_H
 #define LSS_PROFILE_H
 
-#define LSS_MAX_PROFILES 32
+#define LSS_MAX_PROFILES             32
+#define LSS_PROFILE_MAX_CAREERS      32
+#define LSS_PROFILE_CAREER_MAX_TIERS 32
+#define LSS_PROFILE_CAREER_MAX_SONGS 64
+
+typedef struct
+{
+
+	bool song_passed[LSS_PROFILE_CAREER_MAX_SONGS];
+
+} LSS_PROFILE_CAREER_TIER;
+
+typedef struct
+{
+
+	const char * name;
+	LSS_PROFILE_CAREER_TIER tier[LSS_PROFILE_CAREER_MAX_TIERS];
+	int tiers;
+
+} LSS_PROFILE_CAREER;
 
 typedef struct
 {
 
 	ALLEGRO_CONFIG * config;
-	
+
 	/* cached data */
 	const char * name;
+	LSS_PROFILE_CAREER career[LSS_PROFILE_MAX_CAREERS];
+	int careers;
 
 } LSS_PROFILE;
 
